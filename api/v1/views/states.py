62cf57b4c0227653a70edba6816031456094f485
matxa <42968545+matxa@users.noreply.github.com>
2020-09-07 13:41:55 -0400

    task 7 still need to work on delete request

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,14 +6,23 @@ from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
-@app_views.route('/states', methods=['GET', 'POST', 'DELETE', 'PUT'])
+@app_views.route('/states/', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def states():
     json_repr = []
+    if request.method == 'POST':
+        if request.get_json():
+            if 'name' in request.get_json().keys():
+                new_state_instance = State()
+                new_state_instance.name = request.get_json().get('name')
+                new_state_instance.save()
+                storage.save()
+                return jsonify(new_state_instance.to_dict()), 201
+            else:
+                abort(400, "Missing name")
+        else:
+            abort(400, "Not a JSON")
     for v in storage.all(State).values():
         json_repr.append(v.to_dict())
-        if request.method == 'POST':
-            state = request.get_json()
-            return state
     return jsonify(json_repr)
 
 
@@ -25,6 +34,7 @@ def states_id(state_id):
             return selected_state.to_dict()
         if request.method == 'DELETE':
             selected_state.delete()
+            storage.save()
             return jsonify({}), 200
     else:
         abort(404)
