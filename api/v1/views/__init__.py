7baafad8635cf37fd871eb5d3479c15841578125
matxa <42968545+matxa@users.noreply.github.com>
2020-09-09 03:32:33 -0400

    last commit -3am

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,5 @@
 """Creating a flask app Blueprints
 """
 from flask import Blueprint, request, abort, jsonify
-from api.v1.views import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
