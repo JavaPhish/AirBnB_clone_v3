649b479100c7f9df6069b2bff14cbc9a21097d37
JavaPhish <cclements3150@gmail.com>
2020-09-10 18:55:39 -0400

    Removed if statement that had no effect

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -68,19 +68,18 @@ def post_amenites():
 def put_amenites(amenity_id):
     """update amenity instance
     """
-    if request.method == 'PUT':
-        data = request.get_json()
-        if data is None:
-            return make_response(jsonify(error="Not a JSON"), 400)
+    data = request.get_json()
+    if data is None:
+        return make_response(jsonify(error="Not a JSON"), 400)
 
-        ignore_keys = ['id', 'created_at', 'updated_at']
-        for value in storage.all(Amenity).values():
-            if value.id == amenity_id:
-                for k, v in data.items():
-                    if k not in ignore_keys and hasattr(Amenity, k):
-                        setattr(value, k, v)
-                        value.save()
-                        return make_response(jsonify(value.to_dict()), 200)
-                    else:
-                        return abort(404)
-        return abort(404)
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for value in storage.all(Amenity).values():
+        if value.id == amenity_id:
+            for k, v in data.items():
+                if k not in ignore_keys and hasattr(Amenity, k):
+                    setattr(value, k, v)
+                    value.save()
+                    return make_response(jsonify(value.to_dict()), 200)
+                else:
+                    return abort(404)
+    return abort(404)
