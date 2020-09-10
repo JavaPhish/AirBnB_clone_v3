c5cd0ef8bdf437284795ad0f12b9f2c58eceebba
matxa <42968545+matxa@users.noreply.github.com>
2020-09-09 21:23:35 -0400

    amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,6 +1,14 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
-from models.state import State
+from models.amenity import Amenity
 
 
+@app_views.route('/amenities')
+def all_amenites():
+    """list all amenites
+    """
+    all_amenites = []
+    for amenity in storage.all(Amenity).values():
+        all_amenites.append(amenity.to_dict())
+    return make_response(jsonify(all_amenites))
